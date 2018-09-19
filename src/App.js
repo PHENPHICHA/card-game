97546b362d6db465c0ec4e1202794b05a3bba5e3
PHENPHICHA <PHENPHICHA>
2018-09-19 14:19:07 +0700

    Components & States

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import WordCard from './WordCard';
+const word = "Hello";
+class App extends Component {
+  render() {
+    return (
+      <div>{
+  
+        <WordCard value="HLEOL"/>
+      }
+      </div>
+    );
+  }
+}
+
+export default App;
