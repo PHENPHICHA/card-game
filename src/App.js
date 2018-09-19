3cbc1f1443d8924afedb50fca6f4824766ad3af4
PHENPHICHA <PHENPHICHA>
2018-09-19 14:36:03 +0700

    CharacterCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import CharacterCard from './CharacterCard';
 class App extends Component {
   render() {
     return (
       <div  className="App" >
-        Hello {'World'}
+       <CharacterCard value="h"/>
+      <CharacterCard value="i"/>
       
       </div>
     );
