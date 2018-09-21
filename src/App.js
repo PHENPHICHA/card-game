8265ce64d3b417015b29728f5af374e840bd3f58
PHENPHICHA <PHENPHICHA>
2018-09-21 16:15:08 +0700

    Game Logic : เปลี่ยน welcome เป็น helloworld

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
-const word = "WELCOME";
+const word = "HELLOWORLD";
 
 
 class App extends Component {
